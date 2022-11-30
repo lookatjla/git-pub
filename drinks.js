89a740d08544d6551818c9f0400b7aebdb4406cb
Jayla Young <jaylayoung1@gmail.com>
2022-11-30 11:49:45 -0600

    set up database, created /drinks get route

---
+++ drinks.js
@@ -0,0 +1,45 @@
+const drinks = [
+    {
+        name: 'cruddy mary',
+        price: 132,
+        image: 'https://i.imgur.com/Va5iIw5.jpg'
+    },
+    {
+        name: 'index on the beach',
+        price: 68,
+        image: 'https://i.imgur.com/XV2aPa2.jpg'
+    },
+    {
+        name: 'hack & coke',
+        price: 1,
+        image: 'https://i.imgur.com/rLOXFRI.jpg'
+    },
+    {
+        name: 'whiskey-value pair',
+        price: 11,
+        image: 'https://i.imgur.com/a7rmkoS.jpg'
+    },
+    {
+        name: '404 horsemen',
+        price: 202,
+        image: 'https://i.imgur.com/FLucOLr.jpg'
+    },
+    {
+        name: 'if stateMint julep',
+        price: 2,
+        image: 'https://i.imgur.com/yaoK0Dg.jpg'
+    },
+    {
+        name: 'APIPA',
+        price: 43,
+        image: 'https://i.imgur.com/qAhA7pi.jpg'
+    },
+    {
+        name: 'node to joy',
+        price: 56,
+        image: 'https://i.imgur.com/MbVdwZz.jpg'
+    }
+];  // step 1: create array
+
+
+module.exports = drinks;    // step 3: tells us what part of the module (file) we want to make available to files that require() this module (drinks.js)
--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-const express = require('express');
-const app = express();
+const express = require('express'); // 1
+const app = express();  // 2
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Gitpub App!');
-})
+}); // 3
+
+
+app.get('/drinks', (req, res) => {
+    res.send(drinks);
+}); // 6
 
 app.listen(3000, () => {
     console.log('App is listening on port 3000');
-});
+}); // 4
+
+
+
+// 5
+// how to export and require modules with node js; see drinks.js for steps 1 & 3
+
+const drinks = require('./drinks'); // step 2: require() drinks.js file 
+
+console.log(drinks); // step 4: log drinks to the console
+
+
